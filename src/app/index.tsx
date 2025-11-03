6f827f190fcee30f64ff05e755da6c38cb4fd4b8
masa <161901490+bo-masushi@users.noreply.github.com>
2025-11-03 11:27:03 +0900

    画面資材修正

---
+++ src/app/index.tsx
@@ -0,0 +1,53 @@
+import { JSX } from 'react'
+import {View, Text, StyleSheet } from 'react-native'
+
+const Index = (): JSX.Element => {
+    return (
+        <View style={style.container}>
+            <View>
+                <View>
+                    <Text>Memo App</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+            <View>
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2023年10月1日 10:00</Text>
+                </View>
+                <View>X</View>
+            </View>
+
+            <View>
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2023年10月1日 10:00</Text>
+                </View>
+                <View>X</View>
+            </View>
+
+            <View>
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2023年10月1日 10:00</Text>
+                </View>
+                <View>X</View>
+            </View>
+
+            <View>
+                <Text>+</Text>
+            </View>
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default Index
